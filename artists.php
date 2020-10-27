5901a12bc4894259b7fef294ac497143c8285752
Miggy Añover <miggyanover@gmail.com>
2020-10-28 00:25:45 +0800

    Included Symoblism

--- artists.php
+++ artists.php
@@ -1,1 +1,195 @@
-<?php include 'includes/header.php'; ?>
+<?php 
+    $pagetitle = "Artists";
+    include 'includes/header.php'; 
+?>
+
+<main id="artists">
+    <section id="landing">
+        <div class="container">
+            <div id="quote">
+                <img src="assets/img/quot.svg" alt="" height="20px">
+                <h2>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptate</h2>
+                <h3>BY NAME SURNAME</h3>
+            </div>
+        </div>
+    </section>
+    <section id="meet-the-artists">
+        <div class="container">
+            <div class="flex">
+                <div>
+                    <h1>Meet the <br> artists from UGATLahi</h1>
+                </div>
+                <div>
+                    <p>
+                        The moment you open your device to check the news oftentimes the highlight of every political mass movement orrallies that the media shows are the big sculptures that get burned at the end of the event. Usually resembling the likeness of a political figure, these giant papiermâchés are often made by artist collectives and one of them is UGATlahi. Stands for Ugnayan at Galihan ng mga Artistang Tanod ng Lahi is an organization of artists, art enthusiasts, and critics with a common goal of expressing visual resistance against the existing social order in a creative manner.
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        It was established in 1992 with protest artist Mideo Cruz as one of its pioneer members, along with other students of the University of Santo Tomas. It was formed as a response to the increasing number of Filipino students concerned about the role of culture and the visual arts in nation-building. As a visual artists’ organization, they have been creating effigies for each State of the Nation Address since 2001.
+                    </p>
+                </div>
+        </div>
+    </section>
+    <section id="max">
+        <div class="container">
+            <div id="wrapper" class="flex">
+                <div id="max-desc">
+                    <h1>Max Santiago</h1>
+                    <p>
+                        In a 2011 article by the Varsitarian, the University of Santo Tomas' official student newspaper, Max Santiago has stated "Through visual arts we are able to communicate our message clearly through various forms." As one of UGATLahi's pioneer members, the advertising alumnus has been in the protest art community since 1992. Santiago is currently a coordinator for UGATLahi, and often spearheads the effigies that the collective presents during rallies. He is also a cartoonist for Manila Today.
+                    </p>
+                    <div class="assets">
+                        <div>
+                            <h4>Listen to the interview</h4>
+                            <audio controls>
+                                <source src="horse.ogg" type="audio/ogg">
+                                <source src="horse.mp3" type="audio/mp3">
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                        <div>
+                            <h4>HEADLINE NEWS / EXTERNAL LINKS</h4>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="max-profile">
+                    <h2>"Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="jaime-marianne">
+        <div class="container">
+            <div class="flex">
+                <div id="jaime" class="artist">
+                    <img src="assets/img/hero.png" alt="" height="275px">
+                    <h1>Jaime Calma</h1>
+                    <p>Jaime Calma is currently the educational officer of UGATLahi. He was one of the people that helped conceptualize the famous Rodik's Cube effigy, and also headed the production of the DuterTrain effigy back in 2018.</p>
+                    <div class="assets">
+                        <div>
+                            <h4>Listen to the interview</h4>
+                            <audio controls>
+                                <source src="horse.ogg" type="audio/ogg">
+                                <source src="horse.mp3" type="audio/mp3">
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                        <div>
+                            <h4>HEADLINE NEWS / EXTERNAL LINKS</h4>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="marianne" class="artist">
+                    <h1>Marianne Ramirez</h1>
+                    <p>A new member of the organization at the time of interview, Marianne is a student artist, and a cultural worker.</p>
+                    
+                    <div class="assets">
+                        <div>
+                            <h4>Listen to the interview</h4>
+                            <audio controls>
+                                <source src="horse.ogg" type="audio/ogg">
+                                <source src="horse.mp3" type="audio/mp3">
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                        <div>
+                            <h4>HEADLINE NEWS / EXTERNAL LINKS</h4>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                        </div>
+                    </div>
+                    <img src="assets/img/hero.png" alt="" height="275px">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section id="max">
+        <div class="container">
+            <div id="wrapper" class="flex">
+                <div id="max-desc">
+                    <h1>Luigi Almuena</h1>
+                    <p>
+                        Currently the chairperson of UGATLahi, Luigi Almuena dabbles on both digital and traditional art as a form of protest. As the chairperson of UGATLahi, he handles recruitment and heading the production of their works. From murals to animation, Almuena is also a writerfor Manila Today
+                    </p>
+                    <div class="assets">
+                        <div>
+                            <h4>Listen to the interview</h4>
+                            <audio controls>
+                                <source src="horse.ogg" type="audio/ogg">
+                                <source src="horse.mp3" type="audio/mp3">
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                        <div>
+                            <h4>HEADLINE NEWS / EXTERNAL LINKS</h4>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="max-profile">
+                    <h2>"Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    <section id="jaime-marianne">
+        <div class="container">
+            <div class="flex">
+                <div id="jaime" class="artist">
+                    <img src="assets/img/hero.png" alt="" height="275px">
+                    <h1>Selo Villanueva</h1>
+                    <p>
+                        A visual communications graduate from the UP Diliman, Villanueva currently works for Altermediya as a graphic designer and correspondent. Through his art career, he maximizes his skills in art and design, and applies his political advocacies in service to the people. Villanueva has been active in political and advocacy organizations during his student life, notably Panday Sining, UP Artists’ Circle, and STAND UP. He has also worked for the national secretariat of Kabataan Partylist, as a graphic designer and resident artist. Aside from active volunteer work in said organizations, he teaches art for
+                    </p>
+                    <div class="assets">
+                        <div>
+                            <h4>Listen to the interview</h4>
+                            <audio controls>
+                                <source src="horse.ogg" type="audio/ogg">
+                                <source src="horse.mp3" type="audio/mp3">
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                        <div>
+                            <h4>HEADLINE NEWS / EXTERNAL LINKS</h4>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="marianne" class="artist">
+                    <h1>Andrew Lagman</h1>
+                    <p>
+                        An education ocerfor Panday Sining, while also being a member of other mass organizations. Being an educational officer, he ensures that members are learning, and are aligned with the national democratic mass movement, through discussions on socialrealities. Aside from teaching theory and ideas, he also teaches the members how to apply the learnings to one’s self.
+                    </p>
+                    
+                    <div class="assets">
+                        <div>
+                            <h4>Listen to the interview</h4>
+                            <audio controls>
+                                <source src="horse.ogg" type="audio/ogg">
+                                <source src="horse.mp3" type="audio/mp3">
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                        <div>
+                            <h4>HEADLINE NEWS / EXTERNAL LINKS</h4>
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</a>
+                        </div>
+                    </div>
+                    <img src="assets/img/hero.png" alt="" height="275px">
+
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include 'includes/footer.php'; ?>
